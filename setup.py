50310d47d515e9e359b0fff20c5db1e91d263d75
Josh Groeschl <joshua.groeschl@tutanota.com>
2019-11-19 12:26:37 -0800

    Modified setup.py file for the correct application entry points

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            'pyspyke = pyspike.pyspike:main',
+            'pyspyke = pyspyke.pyspyke:main',
         ]
     },
     package_dir={'': 'src'},
