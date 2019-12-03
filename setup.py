001769c98d3e283aeafe19d9d2df8ffd6697cf7d
Josh Groeschl <joshua.groeschl@tutanota.com>
2019-12-03 09:52:36 -0800

    Version number bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setup(
     name='pyspyke',
-    version='0.0.1',
+    version='0.0.2',
     author='Joshua Groeschl',
     author_email='joshua.groeschl@tutanota.com',
     description='Application for consuming memory of linux systems.',
